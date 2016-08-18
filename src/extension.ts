90751b56a9f22ae4018d54d3a997ae427996e916
t.hayamizu <t.hayamizu@outlook.com>
2016-08-19 00:41:09 +0900

    implements extension.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,11 +16,35 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.insertFile', () => {
         // The code you place here will be executed every time your command is executed
+        vscode.window.showInputBox().then((text) => {
+            let dirname = path.dirname(text);
+            if (dirname == ".") {
+                dirname = vscode.workspace.rootPath;
+            }
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+            let filepath:string  =  dirname + "/" +  path.basename(text);
+            fs.readFile(filepath, (err, data)=>{
+                if (err) {
+                    vscode.window.showErrorMessage(err.message);
+                    return;
+                }
+               
+                const editor = vscode.window.activeTextEditor;
+                // check if there is no selection
+                if (editor.selection.isEmpty) {
+                    // the Position object gives you the line and character where the cursor is
+                    const position = editor.selection.active;
+                    vscode.window.activeTextEditor.edit(edit => {
+                    edit.insert(position, data.toString());
+                    
+                    vscode.window.showInformationMessage("insert file: " + filepath + " is success.");
+                 });
+                }
+            });
+        });
+
     });
 
     context.subscriptions.push(disposable);
